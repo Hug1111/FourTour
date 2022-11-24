1a6b234c825cb31ef470325d33a867f28bc3e8a9
dungtuan <104880844+dungtuan@users.noreply.github.com>
2022-11-24 19:51:36 +0700

    Update nguoiDung.php

--- model/nguoiDung.php
+++ model/nguoiDung.php
@@ -3,8 +3,12 @@ function insert_user($name,$address,$tel,$email,$img,$username,$pass,$role){
     $sql="INSERT INTO `nguoidung`(`name`, `address_user`, `tel`, `email`, `image_nd`, `username`, `pass`, `rolr`) VALUES ('$name','$address','$tel','$email','$img','$username','$pass',$role) ";
     pdo_execute($sql);
 }
-function select_user_all(){
-    $sql="SELECT * FROM `nguoidung` WHERE 1";
+function select_user_all($string){
+    $sql="SELECT * FROM `nguoidung` ";
+    if ($string!='') {
+        $sql.=" WHERE name LIKE '%$string%'";
+    }
+    $sql.=" ORDER BY id_user desc";
     $list_user=pdo_query($sql);
     return $list_user;
 }
@@ -19,6 +23,10 @@ function update_user_id($name,$address,$tel,$email,$img,$username,$pass,$role,$i
 }
 function delete_user($id){
     $sql="DELETE FROM `nguoidung` WHERE id_user=$id";
+    pdo_execute($sql);
+}
+function insert_user_form($name,$address,$tel,$email,$username,$pass){
+    $sql="INSERT INTO `nguoidung`( `name`, `address_user`, `tel`, `email`, `username`, `pass`) VALUES ('$name','$address','$tel','$email','$username','$pass')";
     pdo_execute($sql);
 }
 ?>
