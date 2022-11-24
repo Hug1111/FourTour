6625d3bd998e2f5b402747a11f8bc3741378c46e
dungtuan <104880844+dungtuan@users.noreply.github.com>
2022-11-24 19:58:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,214 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once 'model/pdo.php';
+require_once 'model/nguoiDung.php';
+require_once 'model/setting.php';
+require_once 'model/slider.php';
+require_once 'model/diadiem.php';
+require_once 'model/theloai.php';
+require_once 'model/tour.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Four tour</title>
-    <link href="./dist/output.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <link rel="website icon" type="png" href="./image/FOUR TOUR.jpg">
-</head>
-
-<body>
-    <div class="w-full">
-        <div class="container mx-auto my-0 uppercase">
-            <header class="hover:bg-slate-600 hover:text-red-400 bg-transparent">
-                <div class="grid grid-cols-[25%_50%_25%]">
-                    <!-- Logo -->
-                    <div>
-                        <img class="w-6/12 mx-auto my-0" src="./image/Screenshot_20221109_030423.png" alt="">
-                    </div>
-                    <!-- Menu -->
-                    <div>
-                        <ul class="w-full h-full my-auto flex justify-center items-center">
-                            <li class="px-4 flex-col">
-                                <a href="">Home</a>
-                                <p class="w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300"></p>
-                            </li>
-                            <li class="px-4 flex-col group/item"><a href="">Tour</a>
-                                <p
-                                    class="group/edit invisible group-hover/item:visible w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300">
-                                </p>
-                            </li>
-                            <li class="px-4 flex-col group/item relative flex h-full justify-center"><a
-                                    href="">Destination</a>
-                                <p
-                                    class="group/edit invisible group-hover/item:visible w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300">
-                                </p>
-                                <ul
-                                    class="block absolute top-20 bg-white  text-black group/edit invisible group-hover/item:visible group/item  group-hover:opacity-100 transition-all duration-500 border border-gray-200 rounded-lg">
-                                    <li class="sub-dropdown px-7 py-2"><a href=""
-                                            class="group-hover:invisible">dropdown</a>
-                                        <div class="h-full">
-                                            <ul
-                                                class="sub-dropdown-content absolute left-full top-full bg-white w-max  invisible transition-all duration-400 border border-gray-200 rounded-lg">
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li class="sub-dropdown  px-7 py-2"><a href=""
-                                            class="group-hover:invisible">dropdown</a>
-                                        <div class="">
-                                            <ul
-                                                class="sub-dropdown-content absolute left-full top-full bg-white w-max  invisible transition-all duration-400 border border-gray-200 rounded-lg">
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li class="sub-dropdown  px-7 py-2"><a href=""
-                                            class="group-hover:invisible">dropdown</a>
-                                        <div class="h-full">
-                                            <ul
-                                                class="sub-dropdown-content absolute left-full top-full bg-white w-max  invisible transition-all duration-400 border border-gray-200 rounded-lg">
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li class="sub-dropdown px-7 py-2"><a href="" class="group-hover:invisible">chau
-                                            a</a>
-                                        <div class="h-full">
-                                            <ul
-                                                class="sub-dropdown-content absolute left-full top-full bg-white w-max  invisible transition-all duration-400 border border-gray-200 rounded-lg">
-                                                <li><a href="" class="menu-sub-item">thg oat con</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                                <li><a href="" class="menu-sub-item">Sub dropdown menu</a></li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="px-4 flex-col group/item"><a href="">Blog</a>
-                                <p
-                                    class="group/edit invisible group-hover/item:visible w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300">
-                                </p>
-                            </li>
-                            <li class="px-4 flex-col group/item"><a href="">Page</a>
-                                <p
-                                    class="group/edit invisible group-hover/item:visible w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300">
-                                </p>
-                            </li>
-                            <li class="px-4 flex-col group/item"><a href="">Contact</a>
-                                <p
-                                    class="group/edit invisible group-hover/item:visible w-1 mx-auto my-2 rounded-full text-center h-1 bg-orange-300">
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- Icon -->
-                    <div class="flex justify-center items-center pb-3">
-                        <div class="m-3">
-                            <span>0382610702</span>
-                            <i class="fas fa-phone-alt"></i>
-                        </div>
-                        <div class="m-3"><i class="fas fa-search"></i></div>
-                        <div class="m-3 group/item">
-                            <i class="far fa-user"></i>
-                            <div
-                                class="absolute top-20 right-5 border border-gray-200 rounded-b-xl invisible group-hover/item:visible transition-all duration-400">
-                                <div>
-                                    <a class="px-3 py-4" href="">Dang nhap</a>
-                                </div>
-                                <div>
-                                    <a class="px-3 py-4" href="">Dang ki</a>
-                                </div>
-                                <div>
-                                    <a class="px-3 py-4" href="">xem trang ca nhan</a>
-                                </div>
-                                <div>
-                                    <a class="px-3 py-4" href="">Dat tour</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Banner -->
-
-            </header>
-        </div>
-        <hr>
-        <div class="w-full h-[500px] mb-2">
-            <img class="w-full h-full" src="./image/kham-pha-vuon-quoc-gia-Ba-Vi-001.jpg" alt="">
-        </div>
-        <!-- Footer -->
-        <div class="w-10/12 mx-auto my-10">
-            <footer>
-                <div class="grid grid-cols-[75%_25%]">
-                    <div class="grid grid-cols-[50%_25%_25%]">
-                        <div>
-                            <p>Nhung diem den hap dan</p>
-                            <div class="grid grid-cols-2 my-3 py-4">
-                                <p>thg dau buoi tu </p>
-                                <p>thg dau buoi tu </p>
-                                <p>thg dau buoi tu </p>
-                                <p>thg dau buoi tu </p>
-                                <p>thg dau buoi tu </p>
-                                <p>thg dau buoi tu </p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <p class="">cong ty</p>
-                            <div class="my-3 py-4">
-                                <p><a href="">ve chung toi</a></p>
-                                <p><a href="">ve chung toi</a></p>
-                                <p><a href="">ve chung toi</a></p>
-                                <p><a href="">ve chung toi</a></p>
-                            </div>
-                        </div>
-                        <div>
-                            <p class="">Cong dong</p>
-                            <div class="my-3 py-4">
-                                <p><a href="">gia dinh</a></p>
-                                <p><a href="">gia dinh</a></p>
-                                <p><a href="">gia dinh</a></p>
-                                <p><a href="">gia dinh</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <img class="w-7/12 float-right " src="../image/Screenshot_20221109_030423.png" alt="">
-                        <div class="float-right">
-                            <p class="text-orange-400 italic ">Tòa Lotte Center,54 Liễu Giai,Ba Đình,Hà Nội</p>
-                            <div class="float-right">
-                                <p class="float-right">0382610702</p>
-                                <p class="">dungtuan@gmail.com</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <div class="grid grid-cols-3">
-                    <p>© 2022 Zourney.All rights reserved.</p>
-                    <div class="flex justify-center">
-                        <a href="">thg oat con</a>
-                        <a href="">thg oat con</a>
-                        <a href="">thg oat con</a>
-                    </div>
-                    <div class="w-full h-full flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-                    </div>
-
-                </div>
-            </footer>
-        </div>
-    </div>
-</body>
-
-</html>
+//controler
+$setting=select_st();
+$slider=select_all_slider();
+$diadiem=select_all_dd();
+$theloai=select_all_tl();
+$theloai_top5=select_all_tl_top5();
+$tournew=tour_new();
+$danhmuc_diadiem_top_dow=select_top_dow();
+$danhmuc_diadiem_top_up=select_top_up();
+$tour_uudai=tour_uudai();
+$cate=select_all_t();
+include 'view/header.php';
+if (isset($_GET['act'])) {
+    $act=$_GET['act'];
+    switch ($act) {
+        case 'dangki':
+            if (isset($_POST['btn_signup'])) {
+               $name=$_POST['name']; 
+               $address=$_POST['address'];
+               $username=$_POST['username'];
+               $tel=$_POST['tel'];
+               $pass=$_POST['pass'];
+               $repass=$_POST['repass'];
+               $email=$_POST['email'];
+               if ($pass==$repass) {
+               insert_user_form($name,$address,$tel,$email,$username,$pass);
+               $tb='<p class="px-2">Dang ki thanh cong</p>';
+               }
+            }
+            include 'view/dangki.php';
+            break;
+        case 'dangnhap':
+            echo 'hello boi';
+            break;
+        default:
+            include 'view/home.php';
+            break;
+    }
+}
+include 'view/home.php';
+include 'view/footer.php';
+?>
